6e4bc3a1c643185f6d2ec5ed4f82f65431cfb5c8
Julien <juju@macbook-air-de-julien.home>
2021-06-15 15:55:29 +0200

    modif

---
+++ VérifAlphaNum.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Conversion.h"
+
+
+void conversion(char chaine[]){
+	int longueur = strlen(chaine);
+	int curseur = 0;
+	char c;
+	while(curseur < longueur){
+		c = chaine[curseur];
+		if(c == 'é' || c == 'è' || c == 'ë' || c == 'ê'){
+			chaine[curseur] = 'e';
+		}
+		if(c == 'à' || c == 'â' || c == 'ä'){
+			chaine[curseur] = 'a';
+		}
+		if(c == 'ï' || c == 'î'){
+			chaine[curseur] = 'i';
+		}
+		if(c == 'ô' || c == 'ö'){
+			chaine[curseur] = 'o';
+		}
+		if(c == 'ù' || c == 'û' || c == 'ü'){
+			chaine[curseur] = 'u';
+		}
+		if(c == 'ç'){
+			chaine[curseur] = 'c';
+		}
+		if(c == 'Â' || c == 'Ä' || c == 'À'){
+			chaine[curseur] = 'A';
+		}
+		if(c == 'Ê' || c == 'Ë' || c == 'É' || c == 'È'){
+			chaine[curseur] = 'E';
+		}
+		if(c == 'Ç'){
+			chaine[curseur] = 'C';
+		}
+		if(c == 'Ô' || c == 'Ö'){
+			chaine[curseur] = 'O';
+		}
+		if(c == 'Î' || c == 'Ï'){
+			chaine[curseur] = 'I';
+		}
+		curseur++;
+	}
+}
+
