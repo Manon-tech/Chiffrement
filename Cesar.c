6be30c2df3f65ec8b33ccdd692610ab08475a39b
Manon-tech <manon.piquemal@gmail.com>
2021-06-15 16:04:27 +0200

    modification code de cesar

--- Cesar.c
+++ Cesar.c
@@ -3,16 +3,20 @@
 #include <string.h>
 #include "Cesar.h"
 
+
 void cesar(char chaine[], int n){
 	int longueur = strlen(chaine);
 	int curseur = 0;
 	while(curseur < longueur){
 		if(chaine[curseur]>= 'a' && chaine[curseur] <= 'z'){
-			chaine[curseur] += n;
+			chaine[curseur] = 'a' + ((chaine[curseur]-'a')+n) %26;
 		}
 		if(chaine[curseur]>= 'A' && chaine[curseur] <= 'Z'){
-			chaine[curseur] += n;
+			chaine[curseur] = 'A' + (chaine[curseur]-'A'+n) %26;
 		}
-	}
-	curseur++;
+		if(chaine[curseur]>= '0' && chaine[curseur] <= '9'){
+			chaine[curseur] = 'A' + (chaine[curseur]-'0'+n) %26;
+		}
+		curseur++;
+	}s	
 }
