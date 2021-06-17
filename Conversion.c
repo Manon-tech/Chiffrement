dac0bc0e10a093cca231945097c0f3d29c71a290
Manon-tech <manon.piquemal@gmail.com>
2021-06-17 19:49:29 +0200

    modification de conversion.c

--- Conversion.c
+++ Conversion.c
@@ -4,15 +4,13 @@
 #include "Conversion.h"
 
 
+
 void conversion(char chaine[]){
-	char accent[] = {'é','è','ë','ê','à','â','ä','ï','î','ô','ö','ù','û','ü','ç','Â','Ä','À','Ê','Ë','É','È','Ç','Ô','Ö','Î','Ï'};
-	char remplacement[] = {'e','e','e','e','a','a','a','i','i','o','o','u','u','u','c','A','A','A','E','E','E','E','C','O','O','I','I'};
-	int longueur = strlen(chaine);
-	char c;
-	for(int curseur = 0; curseur < longueur; curseur++){
-		c = chaine[curseur];
-		for(int curseur2 = 0; curseur2 < 27; curseur2++){
-			if(c == accent[curseur2]){
+	wchar_t accent[27] = L"éèëêàâäïîôöùûüçÂÄÀÊËÉÈÇÔÖÎÏ";
+	wchar_t remplacement[27] = L"eeeeaaaiioouuucAAAEEEECOOII";
+	for(int curseur = 0; curseur < strlen(chaine); curseur++){
+		for(int curseur2 = 0; curseur2 < wcslen(accent); curseur2++){
+			if(chaine[curseur] == accent[curseur2]){
 				chaine[curseur] = remplacement[curseur2];
 			}
 		}
