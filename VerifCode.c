71abb74166fb2b3fa78d49648cd351987a4a5edb
Julien <julien.adami@etu.iut-tlse3.fr>
2021-06-17 17:40:45 +0200

    VerifCode

---
+++ VerifCode.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int VerifCode(char *code){
+
+    code="012332";
+    for(unsigned i = 0; i < strlen(code); i++){
+        for(unsigned j = 0; j < 10; j++){
+            if(code[i] <1||code[i]>0){
+
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
