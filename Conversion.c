d718d5597f8f8807b6314d29a27515f716622f5b
Julien <julien.adami@etu.iut-tlse3.fr>
2021-06-18 13:54:08 +0200

    fi

--- Conversion.c
+++ Conversion.c
@@ -5,15 +5,16 @@
 
 
 
-void conversion(char chaine[]){
-	wchar_t accent[27] = L"éèëêàâäïîôöùûüçÂÄÀÊËÉÈÇÔÖÎÏ";
-	wchar_t remplacement[27] = L"eeeeaaaiioouuucAAAEEEECOOII";
-	for(int curseur = 0; curseur < strlen(chaine); curseur++){
-		for(int curseur2 = 0; curseur2 < wcslen(accent); curseur2++){
-			if(chaine[curseur] == accent[curseur2]){
-				chaine[curseur] = remplacement[curseur2];
-			}
-		}
-	}
-}
+// Fonction de remplacements des accents
+// Demande un tableau wchar_t
+void replaceAccents(wchar_t *message){
+    wchar_t accents[54] = L"ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÌÍÎÏìíîïÙÚÛÜùúûüÿÑñÇç";
+    wchar_t sansAcc[54] = L"AAAAAAaaaaaaOOOOOOooooooEEEEeeeeIIIIiiiiUUUUuuuuyNnCc";
 
+    for(unsigned i = 0; i < wcslen(message); i++){
+        for(unsigned j = 0; j < 54; j++){
+            if(message[i] == accents[j]){
+                message[i] = sansAcc[j];
+            }
+        }
+    }
